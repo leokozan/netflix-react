f9b9d4890aa7dcca6c4c844c4cfb084dd8d1511a
leokozan <113151772+leokozan@users.noreply.github.com>
2024-03-21 11:20:20 -0300

    Aplicação netflix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ function App() {
   return (
     <div>
       {categories.map((category) => {
-        return <Row title={category.title}/>
+        return <Row
+        key={category.name}
+        name={category.name} 
+        title={category.title}
+        isLarge={category.isLarge}
+        path={category.path}
+        />
       })}
     </div>
   );
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import './Row.css';
-
+import { getData } from '../../api';
 export default function Row(props){
+const[filmes,setFilmes ] = useState()
+const image_host = 'https://image.tmdb.org/t/p/original/';
+    let fetchData = async () => {
+
+        let resutl = getData(props.path)
+        return resutl
+    }
+    useEffect(()=>{
+        let teste = fetchData();
+        teste.then((data)=>{
+            setFilmes(data?.results)
+        })
+    },[])
     return(
         <div>
             {props.title}
+
+            { <div className='imagens'>
+                {filmes?.map((filme)=>{
+                    return <img key={filme?.id} src={image_host + filme?.backdrop_path}></img>
+                })}
+            </div> }
+
         </div>
     )
 }
