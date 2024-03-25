7255b6a52744253e4e8fd7f799355a0373b338b6
leokozan <113151772+leokozan@users.noreply.github.com>
2024-03-25 09:01:59 -0300

    Banner e Nav

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Home from './Home';
 import { categories } from './api';
 import Row from './components/Row/Row';
-
+import Banner from './components/Banner/Banner';
+import Nav from './components/Nav/Nav';
 function App() {
   return (
     <div>
+    <Nav></Nav>
+    <Banner></Banner>
       {categories.map((category) => {
         return <Row
         key={category.name}
