e8ba959ad5753760509a2d386e919f36adf55541
leokozan <113151772+leokozan@users.noreply.github.com>
2024-03-25 08:37:05 -0300

    Editando row

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,30 +1,49 @@
-import { useEffect, useState } from 'react';
-import './Row.css';
-import { getData } from '../../api';
-export default function Row(props){
-const[filmes,setFilmes ] = useState()
-const image_host = 'https://image.tmdb.org/t/p/original/';
+import { useEffect } from 'react'
+import './Row.css'
+import {getData} from '../../api'
+import { useState } from 'react'
+
+export default function Row({title, path, isLarge}) {
+    const[filmes, setFilmes] = useState()
+    let imageHost = 'https://image.tmdb.org/t/p/original/'
+
     let fetchData = async () => {
 
-        let resutl = getData(props.path)
-        return resutl
+        let result = await getData(path)
+
+        return result
     }
-    useEffect(()=>{
-        let teste = fetchData();
-        teste.then((data)=>{
+
+    useEffect( ()=>{
+
+        let consulta = fetchData()
+        
+        consulta.then((data)=>{
+            console.log(data?.results)
             setFilmes(data?.results)
         })
-    },[])
-    return(
-        <div>
-            {props.title}
 
-            { <div className='imagens'>
-                {filmes?.map((filme)=>{
-                    return <img key={filme?.id} src={image_host + filme?.backdrop_path}></img>
+
+    }, [])
+
+    return (
+        <div className='row-container'>
+
+            <h2 className='row-header'>{title}</h2>
+
+            <div className='row-cards'>
+                {filmes?.map( (filme) => {
+                    
+                    return (
+                        <img
+                        className={`movie-card ${isLarge && "movie-card-large"}` }
+                        key={filme.id} 
+                        src={imageHost + (isLarge?filme.backdrop_path:filme.poster_path)} 
+                        alt={filme.name} >
+                        </img>
+                    )                
                 })}
-            </div> }
-
+            </div>
         </div>
     )
 }
